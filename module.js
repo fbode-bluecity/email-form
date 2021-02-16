002c16f8ffd30c47ad4061b690f5d75a080201bf
fbode-bluecity <77629730+fbode-bluecity@users.noreply.github.com>
2021-02-16 17:35:56 +0100

    finished

--- module.js
+++ module.js
@@ -1,48 +1,19 @@
 // Global variables
 const media_base = "https://www.bluecity.dk/media/wysiwyg/";
 
-deb_status = true; // Debugging status
-const debuggy = (msg, type) => {
-    let format_string = `Debuggy: ${msg}`;
-    if (deb_status) {
-        switch (type) {
-            case 0:
-                console.log(format_string);
-                break;
-            
-            case 1:
-                console.warn(format_string);
-                break;
-            
-            case 2:
-                console.error(format_string);
-                break;
+const form_field = document.getElementById("datatrics-box-input-email");
+const form_submit = document.getElementById("btn-submit");
 
-            default:
-                console.error(`Debuggy - type paramter given: ${type}, is non-valid.`);
-
-        }
-    }
-}
-
-const ID_to_DOM = (id_list = []) => {
-    let dom_list = []; let failed = 0;
-    if (id_list.length != 0) {
-        for (let i = 0; i < id_list.length; i++) {
-            if (document.getElementById(id_list[i]) !== null) {
-                dom_list.push(document.getElementById(id_list[i]));
-            } else {
-                failed++;
-                debuggy(`ID['${id_list[i]}'] didn't correspond to a DOM-element`, 1);
-            }
-        }
-        debuggy(`${id_list.length-failed}/${id_list.length} of the IDs, succesfully grabbd.`, 0);
+const validate_email = (e) => {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(e.target.value)) {
+        form_submit.disabled = false
     } else {
-        debuggy("Zero length array given.", 1);
+        form_submit.disabled = true;
     }
+};
 
-    return dom_list;
-}
+form_field.addEventListener("keyup", validate_email);
+form_field.addEventListener("paste", validate_email);
+form_field.addEventListener("input", validate_email);
 
-let elements = ID_to_DOM(["one", "two", "three", "four"]);
-let elements2 = ID_to_DOM();
+

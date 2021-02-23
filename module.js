176885c63e6d4f55dbaac00813cab404541911b2
fbode-bluecity <77629730+fbode-bluecity@users.noreply.github.com>
2021-02-23 21:06:43 +0100

    Module Posted to Staging
    BUG: Flickering at CSS Transition on Input != disabled.

--- module.js
+++ module.js
@@ -1,12 +1,11 @@
-// Global variables
-const media_base = "https://www.bluecity.dk/media/wysiwyg/";
-
+// Dom element variables
 const subs_form = document.getElementById("subscribe-form");
 const form_field = document.getElementById("datatrics-box-input-email");
 const form_submit = document.getElementById("btn-submit");
 const auto_toggle = document.getElementById("auto-toggle");
 
 const validate_email = (e) => {
+    // Regex String for matching input value to email standard format: -----@---.---
     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value)) {
         form_field.style.borderColor = "rgba(19, 144, 181, 0.5)";
         form_submit.disabled = false

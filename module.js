de1329d99327d10b2816e2a1bdd2b92f70d009e9
fbode-bluecity <77629730+fbode-bluecity@users.noreply.github.com>
2021-02-23 20:32:18 +0100

    Update

--- module.js
+++ module.js
@@ -1,19 +1,28 @@
 // Global variables
 const media_base = "https://www.bluecity.dk/media/wysiwyg/";
 
+const subs_form = document.getElementById("subscribe-form");
 const form_field = document.getElementById("datatrics-box-input-email");
 const form_submit = document.getElementById("btn-submit");
+const auto_toggle = document.getElementById("auto-toggle");
 
 const validate_email = (e) => {
     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value)) {
+        form_field.style.borderColor = "rgba(19, 144, 181, 0.5)";
         form_submit.disabled = false
     } else {
+        form_field.style.borderColor = "#cecece";
         form_submit.disabled = true;
     }
 };
 
+const set_transition = (e) => {
+    e.preventDefault();
+    form_submit.className += "submitted";
+    console.log("event fired");
+}
+
 form_field.addEventListener("keyup", validate_email);
 form_field.addEventListener("paste", validate_email);
 form_field.addEventListener("input", validate_email);
-
-
+form_submit.addEventListener("click", set_transtition);
